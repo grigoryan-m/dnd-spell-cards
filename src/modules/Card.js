ac6796d7fc58ab1947a79ef99a718462fee412d3
grigoryan.m <waldm4rk@gmail.com>
2023-09-23 17:27:39 +0400

    first commit

---
+++ src/modules/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Card.css';
+
+function Card(){
+    return(
+        <div className="card">
+            <h2>Frieball</h2>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/modules/Grid.js
@@ -0,0 +1,12 @@
+import '../styles/Grid.css';
+import Card from './Card';
+
+function Grid() {
+  return (
+    <div className="Grid">
+      <Card />
+    </div>
+  );
+}
+
+export default Grid;
