641f655cd7ff1a09033e7bebbe1463d7e26e5367
grigoryan.m <waldm4rk@gmail.com>
2023-09-23 18:27:15 +0400

    user can now add spells

--- src/modules/Grid.js
+++ src/modules/Grid.js
@@ -1,11 +1,50 @@
-import '../styles/Grid.css';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
+import '../styles/Grid.css';
 
 function Grid() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const handleSpacebarPress = (event) => {
+      if (event.keyCode === 32) {
+        console.log("Spacebar pressed");
+        
+        const newCard = {
+          id: Math.random(),
+          title: window.prompt("Spell title (e.g. Fireball)"),
+          level: window.prompt("Spell level (e.g. 3rd)"),
+          castingTime: window.prompt("Casting time (e.g. 1 action)"),
+          range: window.prompt("Spell range (e.g. 150 feet)"),
+          components: window.prompt("Spell components (e.g. V, S, M)"),
+          materials: window.prompt("If there is M component, type it which one is it. If none, leave empty"),
+        };
+
+        setCards([...cards, newCard]);
+      }
+    };
+
+    window.addEventListener('keydown', handleSpacebarPress);
+    return () => {
+      window.removeEventListener('keydown', handleSpacebarPress);
+    };
+  }, [cards]);
+
   return (
-    <div className="Grid">
-      <Card title="Fireball" level="3rd" castingTime="1 action" range="150 feet" components="V, S, M" materials="A tiny ball of bat guano and sulfur"/>
-      <Card title="Fireball" level="3rd" castingTime="1 action" range="150 feet" components="V, S, M" materials="A tiny ball of bat guano and sulfur"/>
+    <div>
+      <div className="Grid">
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            level={card.level}
+            castingTime={card.castingTime}
+            range={card.range}
+            components={card.components}
+            materials={card.materials}
+          />
+        ))}
+      </div>
     </div>
   );
 }
